971e908029fa7e3f48c8b9ece194f92a2f5b63e4
dpwong <daniel.p.wong2@gmail.com>
2014-03-23 22:36:21 -0700

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -18,8 +18,8 @@
 #include "print.h"
 
 Token* get_token(char*);
-BOOLEAN get_source_line(char[]);
 void skip_comment(char[]);
 void skip_blanks(char[]);
+char sourceLine[MAX_TOKEN_STRING_LENGTH];
 
 #endif
